13f4031d3ca79f78a38ffb18158404975c93f23f
Manuella Bernardinelli <manuellabernardinelliborges@gmail.com>
2023-05-17 14:42:37 -0300

    Commitando segunda alteração

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+        System.out.println("Informe seu perfil");
+        String texto = sc.nextLine();
     }
 }
